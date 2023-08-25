335ced63775c80d2343743e944166b573ad9bee0
Jay Kirtlan <jaykirtlan@hotmail.com>
2023-08-25 12:38:30 +0100

    Working on second function

--- app.js
+++ app.js
@@ -4,16 +4,31 @@
 
 
 // below targets the input element and the list element allowing us to use the input field and target the UL 
-const taskInput = getElementById("taskInput")
-const taskList = getElementById("taskList")
+const taskInput = document.getElementById("taskInput")
+const taskList = document.getElementById("taskList")
 
 // add an event listener to the input field, that on pressing the enter key and having anything other than a blank input field will append the text to the list 
 
 taskInput.addEventListener ("keydown", function(e)
 // if the event (e) is equal to the key enterr and the input field does not have a value of an empty string then trim the whitespace at either side then continue the function
 {if (e.key=== "Enter" && taskInput.value.trim() !== "") {
+    // this will prevent the code from refreshing the page on event trigger and clearing the list 
+    e.preventdefault ()
 //add the input value to the task list and trim the whitespace at the start and end of the input
 addTask(taskInput.value.trim())
 // Reset the task input value to nothing once complete.
 taskInput.value = "" }
 })
+
+function addTask(taskText) {
+    const li = document.createElement("li")
+    li.innerHTML = `
+    <span> ${taskText} </span>
+    <button class="deleteBtn> Delete </button>
+    `
+    taskList.appendChild(li)
+
+    li.querySelector (".deleteBtn").addEventListener("click" , function() {
+        taskList.removeChild(li)
+    })
+} 
